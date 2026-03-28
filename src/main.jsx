f029d52d8261fcbd4357d0562c91f76b95811541
matpcul-tech <matpcul@gmail.com>
2026-03-28 16:51:59 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AILTWebsite from './App.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AILTWebsite />
+  </React.StrictMode>,
+)
